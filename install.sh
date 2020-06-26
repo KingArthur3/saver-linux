f9543795bf942d604013c3d5e833f66123502560
Artur Boguslawski <artur@artur.localdomain>
2020-06-26 12:00:50 +0200

    version 1.1

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo echo | grep -q a
-
-localdirectory=`dirname $0`
-sudo cp $localdirectory/saver /usr/bin/saver
-
--- uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo echo | grep -q a
-
-sudo rm -rf /usr/bin/saver
